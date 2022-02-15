612ec409ca01949bde0029d6709a87697f0cd620
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-02-15 19:31:11 +0100

    pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App: React.FC = () => (
-  <div className="underline">BO = 123456789 ---</div>
+  <div className="bg-gray-200 flex items-center justify-center h-screen">Khawla Hobi</div>
 );
 
 export default App;
