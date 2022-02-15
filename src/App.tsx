352a2b0ff1aa54b4bddd7ec06c0e9c2325c26ce3
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-02-15 18:59:25 +0100

    pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,7 @@
-import React, { lazy, Suspense } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-const About = lazy(() => import("./About"));
-const Home = lazy(() => import("./Home"));
+import React from "react";
 
 const App: React.FC = () => (
-  <Router>
-    <Suspense fallback={<div>Loading...</div>}>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Suspense>
-  </Router>
+  <div>BO = 123</div>
 );
 
 export default App;
