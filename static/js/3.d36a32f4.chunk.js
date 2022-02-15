a98a6add2f004cfb0ba563338cdfa86be40c6cf4
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-02-15 19:00:34 +0100

    Updates

--- static/js/3.d36a32f4.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonppwa-react-typescript"]=this["webpackJsonppwa-react-typescript"]||[]).push([[3],{28:function(t,e,a){"use strict";a.r(e);var p=a(0),s=a.n(p);e.default=function(){return s.a.createElement("h1",null,"This is a PWA")}}}]);
-//# sourceMappingURL=3.d36a32f4.chunk.js.map
