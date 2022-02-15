1299a5c951cf0a39735d5e97a89ead7bc7478c60
Taki Eddine RAHAL <taki-eddine.rahal@proxym-it.com>
2022-02-15 19:12:40 +0100

    pwa

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App: React.FC = () => (
-  <div className="underline">BO = 123456789</div>
+  <div className="underline">BO = 123456789 ---</div>
 );
 
 export default App;
