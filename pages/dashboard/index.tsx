0679b8b7bef2eb26a9b1c605765c06e65f8ca8ac
Taki-eddine.RAHAL <takieddinerahal@gmail.com>
2022-07-22 07:58:33 +0100

    Add Multiple envirments

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -4,6 +4,7 @@ import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
 export default function Dashboard(){
+
     return (
         <div>
             <SideBar />
